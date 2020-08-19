092b3d43708aa6cfc1cfcdd7472ccf044318ab9c
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-08-19 13:40:32 +0200

    fixed dependency in setup

--- setup.py
+++ setup.py
@@ -2,26 +2,28 @@ import setuptools
 from setuptools import find_packages
 import signaturizer
 
-setuptools.setup(name='signaturizer',
-                 version=signaturizer.__version__,
-                 description='Generate Chemical Checker signatures from molecules SMILES.',
-                 long_description=open('README.md').read().strip(),
-                 long_description_content_type="text/markdown",
-                 author='Martino Bertoni',
-                 author_email='martino.bertoni@irbarcelona.org',
-                 url='http://gitlabsbnb.irbbarcelona.org/packages/signaturizer',
-                 py_modules=find_packages(),
-                 packages=find_packages(),
-                 install_requires=['tensorflow',
-                                   'tensorflow_hub',
-                                   'numpy',
-                                   'h5py'],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='signaturizer bioactivity signatures chemicalchecker',
-                 classifiers=[
-                     "License :: OSI Approved :: MIT License",
-                     "Programming Language :: Python :: 2",
-                     "Programming Language :: Python :: 3",
-                     "Programming Language :: Python :: 3.7",
-                 ])
+setuptools.setup(
+    name='signaturizer',
+    version=signaturizer.__version__,
+    description='Generate Chemical Checker signatures from molecules SMILES.',
+    long_description=open('README.md').read().strip(),
+    long_description_content_type="text/markdown",
+    author='Martino Bertoni',
+    author_email='martino.bertoni@irbarcelona.org',
+    url='http://gitlabsbnb.irbbarcelona.org/packages/signaturizer',
+    py_modules=find_packages(),
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow<2',
+        'tensorflow_hub',
+        'numpy',
+        'h5py'],
+    license='MIT License',
+    zip_safe=False,
+    keywords='signaturizer bioactivity signatures chemicalchecker',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ])
