55efe8a044710162a20d1a75cb587db85922f6b5
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2021-02-05 11:07:21 +0100

    fixed requirements and version

--- signaturizer/__init__.py
+++ signaturizer/__init__.py
@@ -2,4 +2,4 @@ from .signaturizer import Signaturizer, SignaturizerResult
 
 __author__ = """Martino Bertoni"""
 __email__ = 'martino.bertoni@irbbarcelona.org'
-__version__ = '1.1.9'
+__version__ = '1.1.10'
