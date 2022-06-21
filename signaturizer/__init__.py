4100506dead31a1125c33d3114eb09afcee66acb
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2022-06-21 13:01:40 +0200

    bump version

--- signaturizer/__init__.py
+++ signaturizer/__init__.py
@@ -2,4 +2,4 @@ from .signaturizer import Signaturizer, SignaturizerResult
 
 __author__ = """Martino Bertoni"""
 __email__ = 'martino.bertoni@irbbarcelona.org'
-__version__ = '1.1.11'
+__version__ = '1.1.12'
