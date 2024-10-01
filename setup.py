460a2d2ff79d08fc1ba4c8617c1b3170c7ef8f68
ymartins <ycfrenchgirl2@gmail.com>
2024-10-01 15:03:51 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 __author__ = """Martino Bertoni"""
 __email__ = 'martino.bertoni@irbbarcelona.org'
-__version__ = '1.1.15'
+__version__ = '1.1.16'
 
 setup(
     name='signaturizer',
